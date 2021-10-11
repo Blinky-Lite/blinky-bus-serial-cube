d87bddd9078c607d0b71fba68ec99960f5dc1d35
David McGinnis <dmcginnis427@gmail.com>
2021-10-11 22:30:51 +0200

    first commit

---
+++ cubeCode/cubeCode.ino
@@ -0,0 +1,28 @@
+#include "ModbusRtu.h"
+
+// data array for modbus network sharing
+uint16_t au16data[16] = {1, 1, 1, 1, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0 };
+int ledPin[4] = {13, 2, 3, 4};
+
+/**
+ *  Modbus object declaration
+ *  u8id : node id = 0 for master, = 1..247 for slave
+ *  port : serial port
+ *  u8txenpin : 0 for RS-232 and USB-FTDI 
+ *               or any pin number > 1 for RS-485
+ */
+Modbus slave(1,Serial,0); // this is slave @1 and RS-232 or USB-FTDI
+
+void setup() 
+{
+  for (int ii = 0; ii < 4; ++ii) pinMode(ledPin[ii], OUTPUT);
+
+  Serial.begin( 19200 ); // baud-rate at 19200
+  slave.start();
+}
+
+void loop() 
+{
+  slave.poll( au16data, 16 );
+  for (int ii = 0; ii < 4; ++ii) digitalWrite(ledPin[ii], au16data[ii]);
+}
