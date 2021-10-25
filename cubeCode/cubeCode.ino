81e7d8b524dc018f539dd6a7e1aa34c9028443b9
David McGinnis <david.mcginnis@bl-mc.se>
2021-10-25 11:55:18 +0200

    correct initcube

--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -1,9 +1,8 @@
 #include "ModbusRtu.h"
+#define commLEDPin    13
 
-// data array for modbus network sharing
-uint16_t modbusBuffer[16] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-int ledPin[4] = {13, 2, 3, 4};
-
+boolean commLED = false;
+uint16_t msgCnt = 0;
 /**
  *  Modbus object declaration
  *  u8id : node id = 0 for master, = 1..247 for slave
@@ -12,17 +11,73 @@ int ledPin[4] = {13, 2, 3, 4};
  *               or any pin number > 1 for RS-485
  */
 Modbus slave(1,Serial,0); // this is slave @1 and RS-232 or USB-FTDI
+#define MODBUSBUFSIZE  4
+
+union ModbusUnion
+{
+  struct
+  {
+    uint16_t initCube;
+    uint16_t led1;
+    uint16_t led2;
+    uint16_t led3;
+  };
+  uint16_t modbusBuffer[MODBUSBUFSIZE];
+} mb;
+
+int led1Pin = 2;
+int led2Pin = 3;
+int led3Pin = 4;
+
+void setLeds()
+{
+  digitalWrite(led1Pin, mb.led1);    
+  digitalWrite(led2Pin, mb.led2);    
+  digitalWrite(led3Pin, mb.led3);    
+}
 
 void setup() 
 {
-  for (int ii = 0; ii < 4; ++ii) pinMode(ledPin[ii], OUTPUT);
+  pinMode(commLEDPin,    OUTPUT);
+  digitalWrite(commLEDPin, commLED);    
 
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  pinMode(led3Pin, OUTPUT);
+
+  mb.initCube = 1; //tell tray we need an init
+  mb.led1 = 0;
+  mb.led2 = 0;
+  mb.led3 = 0;
+  setLeds();
+  for (int ii = 0; ii < 10; ++ii)
+  {
+    commLED = !commLED;
+    digitalWrite(commLEDPin, commLED);
+    delay(300);    
+  }
+  commLED = true;
+  digitalWrite(commLEDPin, commLED);
+ 
   Serial.begin( 19200 ); // baud-rate at 19200
   slave.start();
 }
 
 void loop() 
 {
-  slave.poll( modbusBuffer, 16 );
-  for (int ii = 0; ii < 4; ++ii) digitalWrite(ledPin[ii], modbusBuffer[ii]);
+  slave.poll( mb.modbusBuffer, MODBUSBUFSIZE );
+  checkComm();
+  setLeds();
+}
+void checkComm()
+{
+  uint16_t numMessages;
+  numMessages = slave.getInCnt();
+  if (numMessages != msgCnt)
+  {
+    msgCnt = numMessages;
+    commLED = !commLED;
+    digitalWrite(commLEDPin, commLED);    
+  }
+  
 }
